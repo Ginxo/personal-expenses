fbc78bbb84eba3de6ecfaae1d02e4a57bf4715c0
Enrique Mingorance Cano <emingora@redhat.com>
2024-12-26 18:03:28 +0100

    solve deployment issues

--- src/app/queries/categories/usePostCategory.ts
+++ src/app/queries/categories/usePostCategory.ts
@@ -3,11 +3,14 @@ import { postCategory } from '@app/services/movements/categoriesService';
 import { useMutation } from '@tanstack/react-query';
 import { categoriesKeys } from './categoriesKeys';
 import { refetchCategories } from './useFetchCategories';
+import { useFetchAccessToken } from '../users/useFetchToken';
 
 export const usePostCategory = (baseKey: string) => {
+  const { accessToken } = useFetchAccessToken();
+
   const { mutate, data, error, status, reset } = useMutation({
     mutationKey: categoriesKeys.post(baseKey),
-    mutationFn: async (category: Partial<Category>) => await postCategory(category),
+    mutationFn: async (category: Partial<Category>) => await postCategory(category, accessToken!),
     onSuccess: () => refetchCategories(baseKey),
   });
 
